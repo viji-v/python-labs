d3a13bb599e255e038493765aaffc0bcd0d1a969
Vijayalakshmi <vijimay12@gmail.com>
2019-08-21 22:11:30 +0530

    first commit

---
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -0,0 +1,6 @@
+'''
+
+Write the code to calculate how many seconds are in a year in this file,
+so you can execute it as a script and receive the output to your console.
+
+'''
---
+++ 04_conditionals_loops/04_05_sum.py
@@ -0,0 +1,8 @@
+'''
+Take two numbers from the user, an upper and lower bound.
+Using a loop, calculate the sum of all numbers from the lower bound to the upper bound.
+
+For example, if a user enters 1 and 100, the output should be:
+
+The sum is: 5050
+'''
